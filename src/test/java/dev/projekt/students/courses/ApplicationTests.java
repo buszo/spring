d76474432fb707e220f3a5d2fe528c1fb69ea444
Michał Ratuszniak <michal@MacBook-Pro-Micha.local>
2024-12-17 23:38:54 +0100

    Pusty projekt spring

---
+++ src/test/java/dev/projekt/students/courses/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.projekt.students.courses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
